96bf6188251a0959c0e52e8c20fa6e837f3dc85a
yasuper8 <ysuper8@gmail.com>
2016-11-02 22:10:59 -0700

    adds handlebars template

--- handlebars-starter-code/js/app.js
+++ handlebars-starter-code/js/app.js
@@ -62,25 +62,78 @@ $(document).ready(function() {
     // which is nested in the `tracks` object
     var trackResults = spotifyResults.tracks.items;
     console.log('search result tracks:', trackResults);
+    console.log('search result tracks0:', trackResults[0]);
 
-    // hide loading gif
-    $loading.hide();
+
+    if(trackResults.length === 0) {
+      alert("There is no match with the name you input");
+    } else {
+
+
+      // console.log("trackResults :" trackResults.album.images[0].url)
+
+      // hide loading gif
+      $loading.hide();
+
+      var source = $("#spotify-li-template").html();
+      var template = Handlebars.compile(source);
+
+
+      for(var i = 0; i < trackResults.length; i++) {
+        var spotifyTemplate = {
+          albumUrl: trackResults[i].album.images[0].url,
+          songTitle: trackResults[i].album.name,
+          artistName: trackResults[i].artists[0].name,
+          previewUrl: trackResults[i].preview_url
+        }
+        var tempHtml = template(spotifyTemplate)
+        $('#results').append(tempHtml);
+      }
+    }
 
     // show results on page:
     // iterate through results
-    trackResults.forEach(function (result, index) {
-      // use results to construct HTML we want to show
-      //**** GAAAAAH! THERE HAS TO BE A BETTER WAY! ****//
-      var trackHtml = '<div class="row"><div class="col-xs-4">' +
-        '<img src="' + result.album.images[0].url + '" class="img-responsive"></div>' +
-        '<div class="col-xs-8"><p><strong>' + result.name + '</strong> by ' +
-        result.artists[0].name + '</p><p><a href="' + result.preview_url +
-        '" target="_blank" class="btn btn-sm btn-default">Preview ' +
-        '<span class="glyphicon glyphicon-play"></span></a></p></div></div><hr>';
-
-      // append HTML to the view
-      $('#results').append(trackHtml);
-    });
+    // trackResults.forEach(function (result, index) {
+    //   use results to construct HTML we want to show
+    //   **** GAAAAAH! THERE HAS TO BE A BETTER WAY! ****//
+    //   var trackHtml = '<div class="row"><div class="col-xs-4">' +
+    //     '<img src="' + result.album.images[0].url + '" class="img-responsive"></div>' +
+    //     '<div class="col-xs-8"><p><strong>' + result.name + '</strong> by ' +
+    //     result.artists[0].name + '</p><p><a href="' + result.preview_url +
+    //     '" target="_blank" class="btn btn-sm btn-default">Preview ' +
+    //     '<span class="glyphicon glyphicon-play"></span></a></p></div></div><hr>';
+    //   //
+    //
+    //   var source = $("#spotify-li-template").html();
+    //   var template = Handlebars.compile(source);
+    //
+    //
+    //
+    //   var spotifyTemplate = {
+    //     album_url: result.album.images[0].url,
+    //     song_title: result.album.name,
+    //     artist_name: result.artists[0].name,
+    //     preview_url: result.preview_url
+    //   }
+    //
+    //
+    //
+    //   var tmepHtml = template(spotifyTemplate)
+    //     var tempHtml = <div class="row">
+    //                      <div class='col-sx-4'>
+    //                        <img src="{{album-url}}" class="img-responsive"></div>
+    //                        <div class="col-xs-8">
+    //                        <p><strong>{{name}}</strong> by {{artist-name}}</p>
+    //                        <p><a href="{{preview-url}}" target="_blank" class="btn btn-sm btn-default">Preview
+    //                        <span class="glyphicon glyphicon-play"></span>
+    //                        </a></p>
+    //                      </div>
+    //                    </div>
+    //                    <hr>
+    //
+    //   // append HTML to the view
+    //   $('#results').append(tempHtml);
+    // });
   }
 
 });
